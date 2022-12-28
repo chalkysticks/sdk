91374946024f9695c1aea12f1391da338bb8e7f3
Matt Kenefick <keneficksays@gmail.com>
2022-12-27 20:20:14 -0500

    Fix the exports in our SDK

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
 
-export { default as CollectionAuthentication } from '@chalkysticks/sdk-authentication';
-export { default as ModelAuthentication } from '@chalkysticks/sdk-authentication';
-export { default as ModelJwt } from '@chalkysticks/sdk-authentication';
+export { ModelAuthentication } from '@chalkysticks/sdk-authentication';
+export { ModelJwt } from '@chalkysticks/sdk-authentication';
 
-export { default as CollectionBase } from '@chalkysticks/sdk-core';
-export { default as CollectionUser } from '@chalkysticks/sdk-core';
-export { default as Constants } from '@chalkysticks/sdk-core';
-export { default as Core } from '@chalkysticks/sdk-core';
-export { default as Debounce } from '@chalkysticks/sdk-core';
-export { default as EnumBase } from '@chalkysticks/sdk-core';
-export { default as Environment } from '@chalkysticks/sdk-core';
-export { default as EventDispatcher } from '@chalkysticks/sdk-core';
-export { default as ExceptionConnectionRefused } from '@chalkysticks/sdk-core';
-export { default as ExceptionInvalidInput } from '@chalkysticks/sdk-core';
-export { default as ExceptionInvalidRenderer } from '@chalkysticks/sdk-core';
-export { default as ExceptionInvalidStorageProvider } from '@chalkysticks/sdk-core';
-export { default as ExceptionNotImplemented } from '@chalkysticks/sdk-core';
-export { default as ExceptionParserFailure } from '@chalkysticks/sdk-core';
-export { default as ExceptionUnauthorized } from '@chalkysticks/sdk-core';
-export { default as ModelBase } from '@chalkysticks/sdk-core';
-export { default as ModelUser } from '@chalkysticks/sdk-core';
-export { default as StoreProvider } from '@chalkysticks/sdk-core';
+export { default as ChalkySticks } from '@chalkysticks/sdk-core';
+export { CollectionBase } from '@chalkysticks/sdk-core';
+export { CollectionUser } from '@chalkysticks/sdk-core';
+export { Constants } from '@chalkysticks/sdk-core';
+export { Core } from '@chalkysticks/sdk-core';
+export { Debounce } from '@chalkysticks/sdk-core';
+export { EnumBase } from '@chalkysticks/sdk-core';
+export { Environment } from '@chalkysticks/sdk-core';
+export { EventDispatcher } from '@chalkysticks/sdk-core';
+export { ExceptionConnectionRefused } from '@chalkysticks/sdk-core';
+export { ExceptionInvalidInput } from '@chalkysticks/sdk-core';
+export { ExceptionInvalidRenderer } from '@chalkysticks/sdk-core';
+export { ExceptionInvalidStorageProvider } from '@chalkysticks/sdk-core';
+export { ExceptionNotImplemented } from '@chalkysticks/sdk-core';
+export { ExceptionParserFailure } from '@chalkysticks/sdk-core';
+export { ExceptionUnauthorized } from '@chalkysticks/sdk-core';
+export { ModelBase } from '@chalkysticks/sdk-core';
+export { ModelUser } from '@chalkysticks/sdk-core';
+export { StoreProvider } from '@chalkysticks/sdk-core';
 
-export { default as CollectionDiagram } from '@chalkysticks/sdk-pad';
-export { default as ModelDiagram } from '@chalkysticks/sdk-pad';
+export { CollectionDiagram } from '@chalkysticks/sdk-pad';
+export { ModelDiagram } from '@chalkysticks/sdk-pad';
 
-export { default as CollectionPlayer } from '@chalkysticks/sdk-players';
-export { default as ModelPlayer } from '@chalkysticks/sdk-players';
+export { CollectionPlayer } from '@chalkysticks/sdk-players';
+export { ModelPlayer } from '@chalkysticks/sdk-players';
 
-export { default as CollectionSchedule } from '@chalkysticks/sdk-tv';
-export { default as ModelSchedule } from '@chalkysticks/sdk-tv';
+export { CollectionSchedule } from '@chalkysticks/sdk-tv';
+export { ModelSchedule } from '@chalkysticks/sdk-tv';
 
-export { default as CollectionVenue } from '@chalkysticks/sdk-venues';
-export { default as CollectionVenueDetail } from '@chalkysticks/sdk-venues';
-export { default as CollectionVenueMedia } from '@chalkysticks/sdk-venues';
-export { default as CollectionVenueMeta } from '@chalkysticks/sdk-venues';
-export { default as ModelVenue } from '@chalkysticks/sdk-venues';
-export { default as ModelVenueDetail } from '@chalkysticks/sdk-venues';
-export { default as ModelVenueMedia } from '@chalkysticks/sdk-venues';
-export { default as ModelVenueMeta } from '@chalkysticks/sdk-venues';
+export { CollectionVenue } from '@chalkysticks/sdk-venues';
+export { CollectionVenueDetail } from '@chalkysticks/sdk-venues';
+export { CollectionVenueMedia } from '@chalkysticks/sdk-venues';
+export { CollectionVenueMeta } from '@chalkysticks/sdk-venues';
+export { ModelVenue } from '@chalkysticks/sdk-venues';
+export { ModelVenueDetail } from '@chalkysticks/sdk-venues';
+export { ModelVenueMedia } from '@chalkysticks/sdk-venues';
+export { ModelVenueMeta } from '@chalkysticks/sdk-venues';
