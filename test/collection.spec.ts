abcc68934053f17c12a273c9738c9835481eed4f
Matt Kenefick <keneficksays@gmail.com>
2023-03-04 14:34:31 -0500

    .

---
+++ test/collection.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import { ModelAuthentication, CollectionBase } from '../src/index';
+
+/**
+ * CollectionBase Test
+ */
+describe('CollectionBase', () => {
+
+    it('should have length', () => {
+        // const collection: CollectionBase = new CollectionBase();
+
+        // collection.add(new ModelBase);
+        // collection.add(new ModelBase);
+        // collection.add(new ModelBase);
+        // collection.add(new ModelBase);
+        // collection.add(new ModelBase);
+
+        // expect(collection.length).to.equal(5);
+    });
+
+});
