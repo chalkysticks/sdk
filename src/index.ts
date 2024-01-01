aaae9f2d8a22adaf527e6d9e368042ea1c0bc41d
Matt Kenefick <keneficksays@gmail.com>
2024-01-01 13:14:10 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import * as Authentication from '@chalkysticks/sdk-authentication';
-import * as Core from '@chalkysticks/sdk-core';
-import * as Pad from '@chalkysticks/sdk-pad';
-import * as Players from '@chalkysticks/sdk-players';
-import * as Tv from '@chalkysticks/sdk-tv';
-import * as Venues from '@chalkysticks/sdk-venues';
-import * as Wallet from '@chalkysticks/sdk-wallet';
+// import * as Authentication from '@chalkysticks/sdk-authentication';
+// import * as Core from '@chalkysticks/sdk-core';
+// import * as Pad from '@chalkysticks/sdk-pad';
+// import * as Players from '@chalkysticks/sdk-players';
+// import * as Tv from '@chalkysticks/sdk-tv';
+// import * as Venues from '@chalkysticks/sdk-venues';
+// import * as Wallet from '@chalkysticks/sdk-wallet';
 
 export * as Authentication from '@chalkysticks/sdk-authentication';
 export * as Core from '@chalkysticks/sdk-core';
 export * as Pad from '@chalkysticks/sdk-pad';
 export * as Players from '@chalkysticks/sdk-players';
-export * as Tv from '@chalkysticks/sdk-tv';
+export * as TV from '@chalkysticks/sdk-tv';
 export * as Venues from '@chalkysticks/sdk-venues';
 export * as Wallet from '@chalkysticks/sdk-wallet';
 
