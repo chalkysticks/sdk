febd3a64f32c714ebe742273492c9df49ca181a7
Matt Kenefick <keneficksays@gmail.com>
2024-01-01 13:14:17 -0500

    .

--- build/esm/index.js
+++ build/esm/index.js
@@ -2,7 +2,7 @@ export * as Authentication from '@chalkysticks/sdk-authentication';
 export * as Core from '@chalkysticks/sdk-core';
 export * as Pad from '@chalkysticks/sdk-pad';
 export * as Players from '@chalkysticks/sdk-players';
-export * as Tv from '@chalkysticks/sdk-tv';
+export * as TV from '@chalkysticks/sdk-tv';
 export * as Venues from '@chalkysticks/sdk-venues';
 export * as Wallet from '@chalkysticks/sdk-wallet';
 export { ChalkySticks } from '@chalkysticks/sdk-core';
